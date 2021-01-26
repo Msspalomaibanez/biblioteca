bb6f9d1fd93b2d9164e47dd124c732189a89e9a8
Paloma <pibanezm04@educantabria.es>
2021-01-26 16:36:54 +0100

    buscarSocio

--- src/entidades/Socio.java
+++ src/entidades/Socio.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
  * @author USER
  */
 public class Socio {
+
     private long id;
     private String nombre;
     private String apellidos;
@@ -20,17 +21,15 @@ public class Socio {
     private String telefono;
     private String direccion;
     private boolean penalizacion = false;
-    private ArrayList <Evento> eventos = new ArrayList <Evento>(); // relación socio y evento
-    private ArrayList <Prestamo> prestamos = new ArrayList <Prestamo>(); // relación socio con préstamo
+    private ArrayList<Evento> eventos = new ArrayList<Evento>(); // relación socio y evento
+    private ArrayList<Prestamo> prestamos = new ArrayList<Prestamo>(); // relación socio con préstamo
 
     /*constructor por defecto*/
-    
     public Socio() {
     }
-    
+
     /*constructor con todos los atributos*/
-    
-    public Socio(long id, String nombre, String apellidos, String dni, String telefono, String direccion, boolean penalizacion, ArrayList <Prestamo> prestamos, ArrayList <Evento> eventos) {
+    public Socio(long id, String nombre, String apellidos, String dni, String telefono, String direccion, boolean penalizacion, ArrayList<Prestamo> prestamos, ArrayList<Evento> eventos) {
         this.id = id;
         this.nombre = nombre;
         this.apellidos = apellidos;
@@ -41,10 +40,9 @@ public class Socio {
         this.eventos = eventos;
         this.prestamos = prestamos;
     }
-    
-        /*constructor de copia*/
-    
-        public Socio(Socio soc) {
+
+    /*constructor de copia*/
+    public Socio(Socio soc) {
         this.id = soc.id;
         this.nombre = soc.nombre;
         this.apellidos = soc.apellidos;
@@ -57,7 +55,6 @@ public class Socio {
     }
 
     /*constructor con los atributos más importantes*/
-        
     public Socio(long id, String nombre, String apellidos, String dni, String telefono, String direccion) {
         this.id = id;
         this.nombre = nombre;
@@ -68,7 +65,6 @@ public class Socio {
     }
 
     /*getters y setters de todos los atributos*/
-    
     public long getId() {
         return id;
     }
@@ -148,44 +144,87 @@ public class Socio {
     public void setEventos(ArrayList<Evento> eventos) {
         this.eventos = eventos;
     }
-    
-   public static Socio nuevoSocio() {
-       Socio soc = new Socio();
-       Scanner in = new Scanner (System.in);
 
-       
-       System.out.println("Introduzca el nombre del nuevo socio: ");
-       soc.setNombre(in.nextLine());
-       System.out.println("Introduzca el apellido del nuevo socio: ");
-       soc.setApellidos(in.nextLine());
-       System.out.println("Introduzca el dni del nuevo socio: ");
-       soc.setDni(in.nextLine());
-       System.out.println("Introduzca el teléfonoo del nuevo socio: ");
-       soc.setTelefono(in.nextLine());
-       System.out.println("Introduzca la dirección del nuevo socio: ");
-       soc.setDireccion(in.nextLine());
-       
-       return soc;
-   }
+    public static Socio nuevoSocio() {
+        Socio soc = new Socio();
+        Scanner in = new Scanner(System.in);
 
-   
-   public static void verSocio (ArrayList<Socio> socios) {
-       System.out.println("El programa tiene almacenados los siguientes socios: ");
-       for (Socio s : socios) {
-           System.out.println(s.getId() + ": " + s.getNombre() + " " + s.getApellidos() );
-       }
-       
-       
-       
-   }   
-   
-   
-   
-   
-    @Override
-    public String toString() {
-        return "Socio{" + "id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", dni=" + dni + ", telefono=" + telefono + ", direccion=" + direccion + ", penalizacion=" + penalizacion + ", eventos=" + eventos + ", prestamos=" + prestamos + '}';
+        System.out.println("Introduzca el nombre del nuevo socio: ");
+        soc.setNombre(in.nextLine());
+        System.out.println("Introduzca el apellido del nuevo socio: ");
+        soc.setApellidos(in.nextLine());
+        System.out.println("Introduzca el dni del nuevo socio: ");
+        soc.setDni(in.nextLine());
+        System.out.println("Introduzca el teléfonoo del nuevo socio: ");
+        soc.setTelefono(in.nextLine());
+        System.out.println("Introduzca la dirección del nuevo socio: ");
+        soc.setDireccion(in.nextLine());
+
+        return soc;
     }
-  
+
+    public static void verSocio(ArrayList<Socio> socios) {
+        System.out.println("El programa tiene almacenados los siguientes socios: ");
+        for (Socio s : socios) {
+            System.out.println(s.getId() + ": " + s.getNombre() + " " + s.getApellidos());
+        }
+       
+       
+       
+
+    public static void buscarSocio(ArrayList<Socio> socios) {
+        Socio buscado;
+        ArrayList<Socio> encontrados;
+        Scanner in;
+        int opcion = -1;
+
+        do {
+            buscado = null;
+            encontrados = new ArrayList<Socio>();
+            in = new Scanner(System.in, "ISO-8859-1");
+            System.out.println("Pulse 1 para buscar socio por ID.");
+            System.out.println("Pulse 2 para buscar socio por NOMBRE.");
+            System.out.println("Pulse 3 para buscar socio por TELEFONO.");
+            System.out.println("Pulse 0 para VOLVER.");
+            
+            opcion = in.nextInt();
+            
+            if(opcion < 0 || opcion > 4) {
+                
+                System.out.println("Opción incorrecta");
+                continue;
+                
+            }
+            in = new Scanner(System.in, "ISO-8859-1");
+            switch (opcion) {
+                case 1:
+                    System.out.println("Por favor introduzca el ID del socio que desea buscar: ");
+                    int idSoc = in.nextInt();
+                    buscado = Socio.
+            }
+        
+        
+        
+        }
+           
+        }
+        public static Socio buscarSocioPorId(int idSocio, ArrayList<Socio> socios) {
+        Socio ret = null;
+        for (Socio s : socios) {
+            if (s.getId() == idSocio) {
+                ret = s;
+                break;
+            }
+        }
+        return ret;
+       
+    }
+
 }
 
+@Override
+        public String toString() {
+        return "Socio{" + "id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", dni=" + dni + ", telefono=" + telefono + ", direccion=" + direccion + ", penalizacion=" + penalizacion + ", eventos=" + eventos + ", prestamos=" + prestamos + '}';
+    }
+
+}
