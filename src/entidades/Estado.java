f001f609a0428e4834f7f6b35b785f80ded82001
Paloma <pibanezm04@educantabria.es>
2021-01-29 12:59:01 +0100

    función convertir array de Estado en ArrayList de Estado

--- src/entidades/Estado.java
+++ src/entidades/Estado.java
@@ -5,6 +5,7 @@
  */
 package entidades;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -12,14 +13,12 @@ import java.util.Scanner;
  * @author USER
  */
 public class Estado {
-    
+
     private String devolucion;
-    
 
     public Estado() {
     }
-    
-    
+
     public Estado(String devolucion) {
         this.devolucion = devolucion;
     }
@@ -31,46 +30,59 @@ public class Estado {
     public void setDevolucion(String devolucion) {
         this.devolucion = devolucion;
     }
-    
+
     public static Estado nuevoEstado() {
         Estado est = new Estado();
         Scanner in = new Scanner(System.in);
         char opcion = '-';
-        
-        if (opcion != 'N' && opcion != 'S' && opcion != 'n' && opcion != 's'){
-        System.out.println(" Indique el estado del préstamo (pendiente, parcial, devuelto): ");
-        String estado = in.nextLine();
-        
-        switch (estado) {
-            case "pendiente":
-                System.out.println("El préstamo se encuentra pendiente de la devolución");
-            break;
-            
-            case "parcial":
-                System.out.println("El préstamo esta devuelto parcialmente");
-                break;
-                
-            case "devuelto":
-                System.out.println("El préstamo ya ha sido devuelto");
-                break;
-                   
-            default:
-                System.out.println("No ha introducido ninguno de los términos exigidos");
-        }
-        
+
+        if (opcion != 'N' && opcion != 'S' && opcion != 'n' && opcion != 's') {
+            System.out.println(" Indique el estado del préstamo (pendiente, parcial, devuelto): ");
+            String estado = in.nextLine();
+
+            switch (estado) {
+                case "pendiente":
+                    System.out.println("El préstamo se encuentra pendiente de la devolución");
+                    break;
+
+                case "parcial":
+                    System.out.println("El préstamo esta devuelto parcialmente");
+                    break;
+
+                case "devuelto":
+                    System.out.println("El préstamo ya ha sido devuelto");
+                    break;
+
+                default:
+                    System.out.println("No ha introducido ninguno de los términos exigidos");
+            }
+
         } else {
-      
-        System.out.println("No ha introducido ninguno de los términos exigidos");
-    }
-        
+
+            System.out.println("No ha introducido ninguno de los términos exigidos");
+        }
+
         return est;
+    }
+
+    /**
+     * Se trata de una función que convierte un arra de objetos tipo Estado en
+     * un ArrayList de objetos de tipo Estado con los mismos elementos del array
+     *
+     * @param array de Estados
+     * @return ArrayList de Estados
+     */
+    public static final ArrayList<Estado> convertir(Estado[] array) {
+        ArrayList<Estado> ret = new ArrayList<Estado>();
+        for (Estado e : array) {
+            ret.add((Estado) e);
+        }
+        return ret;
     }
 
     @Override
     public String toString() {
         return "Estado{" + "devolucion=" + devolucion + '}';
     }
-    
-    
-    
+
 }
