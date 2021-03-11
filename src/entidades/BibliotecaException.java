6e7245903b2a2947ba9a2258de44c2f38c5e861d
Paloma <pibanezm04@educantabria.es>
2021-03-11 19:55:06 +0100

    creación BibliotecaException

---
+++ src/entidades/BibliotecaException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author USER
+ */
+public class BibliotecaException extends Exception {
+    
+    public BibliotecaException(String msg) {
+        super (msg);
+    }
+    
+}
